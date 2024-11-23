2fa67992df7aa93f5c4074a4dffe470ad9217b3b
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-23 14:04:32 -0500

    Pair Programming with Andrew

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,6 +4,8 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+
+  // WORD IS NOT PRESENT 
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -20,7 +22,8 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  // WORD IS HORIZONTALLY PRESENT 
+  it("should return true if the word is horizontally present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +38,32 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+    // WORD IS VERTICALLY PRESENT 
+  it("should return true if the word is vertically present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'F', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'R', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'A', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'N', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'K', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+      // EMPTY ARRAY
+    it("should return false if the array is empty", function() {
+      const result = wordSearch([
+        [],
+      ], 'FRANK')
+    
+      assert.isFalse(result);
+    });
+
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(letters) {
+  const transposed = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    transposed[i] = [];
+    for (let j = 0; j < letters.length; j++) {
+      transposed[i][j] = letters[j][i];
     }
-}
+  }
+  return transposed;
+};
 
-module.exports = wordSearch
+// check horizontally
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+  // check vertically
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
